1ced0b8cae0e602ff437e59a45a4c0fc3402aee7
Jesse Fox-Allen <indispirit@gmail.com>
2026-02-18 05:42:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-139117361-c9162",
+  "appId": "1:650268877706:web:aba58ade6264c44e249e73",
+  "apiKey": "AIzaSyAbCgPfHrdo-YLDnFszNiXWq6Wjn4eONzg",
+  "authDomain": "studio-139117361-c9162.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "650268877706"
+};
