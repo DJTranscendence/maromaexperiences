454f5748472eb9ab37f25311bc546531dde8867e
Jesse Fox-Allen <indispirit@gmail.com>
2026-02-18 05:04:52 +0000

    Try fixing this error: `Runtime ReferenceError: Footer is not defined. E

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
-
 "use client";
 
 import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
